1aa868940eb2230d0e543dc6bb96143dcb70087b
PaulNguyen <paulnguyen2207@gmail.com>
2022-01-15 17:30:48 +1100

    Completed Assigmnet

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import TwitterMessage from "./components/TwitterMessage";
 class App extends Component {
 
   login = ({ username, password }) => {
-    console.log(`Logging in ${username} with password ${password}`);
+    console.log(`Logging in ${username} with password ${password}`
+    );
   };
+
+  
 
   render() {
     return (
@@ -20,9 +23,7 @@ class App extends Component {
         <h1>
           <pre>TwitterMessage</pre>
         </h1>
-        <TwitterMessage maxChars={280} />
-
-
+        <TwitterMessage maxChars={280}/>
 
       </div>
     )
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -3,15 +3,22 @@ import React from "react";
 class TwitterMessage extends React.Component {
   constructor() {
     super();
+    this.state = {message:''}
+  } 
 
-    this.state = {};
+  handleChange=(event)=>{
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
+    let charNumber=this.props.maxChars-this.state.message.length;
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={this.handleChange} type="text" name="message" id="message" value={this.state.message} />
+        {charNumber}
       </div>
     );
   }
